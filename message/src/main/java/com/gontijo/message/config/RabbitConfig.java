a12a1263bdb86ab0095ca5d2aadfd0e45605f0c9
João Victor Gontijo <joao.victor@eprecise.com.br>
2024-09-30 23:02:56 -0300

    feature: criação do projeto para consumo de mensagens

---
+++ message/src/main/java/com/gontijo/message/config/RabbitConfig.java
@@ -0,0 +1,14 @@
+package com.gontijo.message.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    @Bean
+    public Queue userQueue() {
+        return new Queue("userQueue", true);
+    }
+}
